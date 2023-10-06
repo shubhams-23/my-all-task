253074e30caa29edfc89e1c8a4c32897fc90fda4
shubhams@23 <shubhamsharma71845@gmail.com>
2023-10-06 15:02:52 +0530

    Task 1 Date 05-10-2023

---
+++ javascript/0510task.js
@@ -0,0 +1,27 @@
+// Task 1 for multiply two numbers without multiplication
+// Date 05-10-2023
+
+
+ let result =0;
+function mul(a,b){
+    if(a<b){
+        for(i=1;i<=b;i++){
+            result=result+a;
+            
+        }
+         return result;
+            }
+    else{
+        for(i=1;i<=a;i++)
+        {
+            result=result+b;
+         }
+         return result;
+        }
+    }
+mul(2,3); 
+
+
+// Task 2 Write a program to remove duplicate from an Array
+// Date 05-10-2023
+
